15902b8a8c38b5dcb3a29651bac6ec302cc20d3e
mark.devol <mark.a.devol.ctr@army.mil>
2024-03-04 11:20:16 -0700

    Added database tables and migrations

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,13 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int NumberOfDays { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public LeaveType LeaveType { get; set; }
+    }
+}
